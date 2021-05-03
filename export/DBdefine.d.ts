766d63864a8de3e99615c1acc8aba9264085e4f8
xingwy <yuxingwei.code@foxmail.com>
2021-05-03 16:55:13 +0800

    优化数据模型导出文件

--- export/DBdefine.d.ts
+++ export/DBdefine.d.ts
@@ -1,12 +1,21 @@
-const enum AccountFields {
-    account = 0,  // 账号
-    password = 1,  // 密码
-    uid = 2,  // 用户唯一ID
-}
-type Account = [string, string, string];
+/**
+ * 数据库字段段映射模型文件
+ */
 
-const enum AccountsFields {
-    uid = 0,  // 用户唯一ID
-}
-type Accounts = [Array<Account>];
+ declare namespace DBModel {
 
+    // 账号模型
+    const enum AccountFields {
+        account = 0,  // 账号
+        password = 1,  // 密码
+        uid = 2,  // 用户唯一ID
+    }
+    type Account = [string, string, string];
+
+    // 账号模型集合
+    const enum AccountsFields {
+        uid = 0,  // 用户唯一ID
+    }
+    type Accounts = [Array<Account>];
+
+}
--- src/models/export.ts
+++ src/models/export.ts
@@ -1,18 +1,25 @@
-import * as DBModel from "./dbModel";
 import * as path from "path";
+import * as DBModel from "./dbModel";
 import { Type } from "../struct/type";
 import { write } from "../util/write";
 
 
 const createFromType = (type: Type): string => {
     // 组成fields
+
+    let desStr = "";
     let fieldStr = "";
     let typeStr = "";
-    fieldStr += `const enum ${type.name}Fields {\n`
-    typeStr += `type ${type.name} = [`;
+    fieldStr += `    const enum ${type.name}Fields {\n`
+    typeStr += `    type ${type.name} = [`;
+
+    if (type.des) {
+        desStr += `    // ${type.des}\n`;
+    }
+
     for (let index = 0; index < type.childsType.length; index++) {
         let child = type.childsType[index];
-        fieldStr +=`    ${child.name} = ${index},`;
+        fieldStr +=`        ${child.name} = ${index},`;
         if (child.des) {
             fieldStr += `  // ${child.des}`;
         }
@@ -29,20 +36,31 @@ const createFromType = (type: Type): string => {
     if (offset != -1) {
         typeStr = typeStr.substring(0, typeStr.length-2);
     }
-    fieldStr += `}\n`;
+    fieldStr += `    }\n`;
     typeStr += `];\n\n`;
-    return fieldStr + typeStr;
+
+    
+    return desStr + fieldStr + typeStr;
 }
 
 export const run = async () => {
     try {
+         // 包装头、尾
+        let headStr = "";
+        let tailStr = "";
+        headStr += `/**\n`;
+        headStr += ` * 数据库字段段映射模型文件\n`;
+        headStr += ` */\n\n`;
+        headStr += ` declare namespace DBModel {\n`;
+        headStr += `\n`;
         // 导出数据库结构模型
         let content = "";
         Object.values(DBModel).forEach((v: Type) => {
             content += createFromType(v);
         })
+        tailStr += `}`;
         let dir = path.join(__filename, "../../../export/DBdefine.d.ts");
-        await write(dir, content);
+        await write(dir, headStr + content + tailStr);
         console.info(`已经导出文件：${dir}`);
     } catch (e) {
         console.error(`导出数据结构模型出错，error:${e.message}, stack:${e.stack}`);
